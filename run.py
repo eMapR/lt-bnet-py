69633e21653c41bfd984b06d46448640c0cd28fe
RustyShuckleford <peterclary87@gmail.com>
2024-11-17 13:22:40 -0800

    cleaned up run.py a little. unchecked

--- run.py
+++ run.py
@@ -333,14 +333,9 @@ def export_feature_collection(fc,asset_id,asset_path):
 	return fc_task
 
 
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
-
-
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
+###########################################################################################################################
+## 
+###########################################################################################################################
 def classifier(self, labeled_fc_path, unlabeled_fc_path, label_property, num_trees=200):
 	"""
 	Initialize the FeatureClassifier with the required parameters.
@@ -358,6 +353,9 @@ def classifier(self, labeled_fc_path, unlabeled_fc_path, label_property, num_tre
 	self.labeled_fc = self.drop_null_features(self.labeled_fc,'tcw_ftv_6')
 	self.unlabeled_fc = self.drop_null_features(self.unlabeled_fc,'tcw_ftv_6')
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def drop_null_features(fc, property_name):
 	"""
 	Drops features from a Feature Collection if they contain null values for a specific property.
@@ -373,11 +371,16 @@ def drop_null_features(fc, property_name):
 	filtered_fc = fc.filter(ee.Filter.notNull(property_name))
 	return filtered_fc
 
-# Run the check before classification
+###########################################################################################################################
+## 
+###########################################################################################################################
 def _mutate_predictor_variables_list(__predictor_variables):
 	__predictor_variables = __predictor_variables.filter(ee.Filter.neq('item', 'system:index')) 
 	return __predictor_variables
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def train_classifier(_labeled_fc,_label_property,_predictor_variables,_num_trees):
 	"""
 	Train a Random Forest classifier using the labeled data.
@@ -392,6 +395,9 @@ def train_classifier(_labeled_fc,_label_property,_predictor_variables,_num_trees
 	return classifier
 
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def classify_features(_unlabeled_fc,_classifier):
 	"""
 	Classify the unlabeled feature collection.
@@ -416,6 +422,9 @@ def classify_features(_unlabeled_fc,_classifier):
 
 
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def print_classified_features(self, classified_fc, limit=5):
 	"""
 	Print the first few classified features.
@@ -427,11 +436,10 @@ def print_classified_features(self, classified_fc, limit=5):
 
 
 
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
 
-
+###########################################################################################################################
+## 
+###########################################################################################################################
 #### Function to convert GeoJSON features to EE Features
 def geojson_to_ee_feature(geojson,s_crs,t_crs):
 	features = []
@@ -448,6 +456,9 @@ def geojson_to_ee_feature(geojson,s_crs,t_crs):
 	# Return a FeatureCollection from the list of EE Features
 	return ee.FeatureCollection(features)
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def reproject_geojson(ft_geojson, src_epsg, target_epsg):
 	"""
 	Reproject the coordinates of a GeoJSON feature from the source EPSG to the target EPSG.
@@ -474,6 +485,9 @@ def reproject_geojson(ft_geojson, src_epsg, target_epsg):
 
 	return ft_geojson
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def process_feature(index, f_list, src_epsg, target_epsg):
 	# Convert the feature from GEE to a Python dict
 	feature = ee.Feature(f_list.get(index)).getInfo()  # Get feature and convert to Python dict
@@ -488,6 +502,9 @@ def process_feature(index, f_list, src_epsg, target_epsg):
 		}
 
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def feature_collection_to_geojson(fc, src_epsg, target_epsg):
 	# Convert GEE FeatureCollection to a Python List object
 	f_list = fc.toList(fc.size())
@@ -516,6 +533,9 @@ def feature_collection_to_geojson(fc, src_epsg, target_epsg):
 
 
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 # Function to monitor the status of multiple tasks and update in place
 def monitor_tasks(tasks):
 	tasks = [i for i in tasks if i != 0]
@@ -544,6 +564,9 @@ def monitor_tasks(tasks):
 
 
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def rasterize_polygons(feature_collection, property_name, scale, region):
 	# Create an empty image to burn the values into
 	#empty_image = ee.Image()#.byte()
@@ -558,6 +581,9 @@ def rasterize_polygons(feature_collection, property_name, scale, region):
 	# Return the rasterized image
 	return rasterized
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def filter_by_mode_value(feature_collection, low, lowmed, medhigh, high):
 	# Filter the collection by the 'classification' property
 	filtered_collection_low = feature_collection.filter(
@@ -581,6 +607,9 @@ def filter_by_mode_value(feature_collection, low, lowmed, medhigh, high):
 	return filtered_collection_out
 
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def buffer_features(feature_collection, buffer_distance):
 	# Define a function to buffer a single feature
 	def buffer_feature(feature):
@@ -597,6 +626,9 @@ def buffer_features(feature_collection, buffer_distance):
 	return buffered_collection
 
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def list_and_delete_assets(asset_path):
 	# List the assets in the specified folder or collection
 	asset_list = ee.data.listAssets({'parent': asset_path})['assets']
@@ -629,8 +661,10 @@ def list_and_delete_assets(asset_path):
 			else:
 				print(f"Skipped asset: {asset['name']}")
 				print("Finished processing all assets.")
-	
 
+###########################################################################################################################
+## 
+###########################################################################################################################
 def export_image_to_asset(image, asset_id, description,scale, region, max_pixels=1e13):
 	"""
 	Export an image to an Earth Engine asset.
@@ -655,67 +689,4 @@ def export_image_to_asset(image, asset_id, description,scale, region, max_pixels
 	print(f"Exporting image to {asset_id} with task ID: {task.id}")
 
 
-
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------
-# generate training datasets --------------------------------------------------
-def generate_training_reference_imagery_and_polygons(lt,image_processor,polygon_generator):
-	# Export the image to GEE assets
-	task1 = image_processor.export_image()
-	# Generate and export disturbance polygons
-	task2 = polygon_generator.export_polygons()
-	return [task1,task2]
-#---------------------------------------------------------------
-def generate_current_imagery_and_polygons(lt,image_processor,polygon_generator):
-	# Export the image to GEE assets
-	task3 = image_processor.export_image()
-	# Generate and export disturbance polygons
-	task4 = polygon_generator.export_polygons()
-	return [task3,task4]
-#---------------------------------------------------------------
-def attribute_training_disturbance_polygons(poly_attr,change_params,asset_path):
-	# attribute polygons one (early) with referance data
-	event_polygons_attri = poly_attr.attribute_with_reference_data()              # apply attribution
-	# reporject polgyons one 4326 to 5070
-	reprojector = bnet.PolygonReprojector()           
-	# Convert FeatureCollection to GeoJSON with reprojection
-	src_epsg = "EPSG:4326"                                                                                                # define projection
-	target_epsg = "EPSG:5070"                                                                                             # define projection
-	# Convert FeatureCollection to GeoJSON with reprojection
-	reprojected_geojson = bnet.feature_collection_to_geojson(event_polygons_attri, reprojector, src_epsg, target_epsg)    # apply reprojections and feature collection to geojson tranfermation
-	# attribute polygon one with disturbance labels (Cmonster)
-	event_polygons_attri1 = poly_attr.attribute_with_cmonster_data(reprojected_geojson)                   # apply attribution (cMonster)
-	reprojected_geojson = bnet.geojson_to_ee_feature(event_polygons_attri1, reprojector, target_epsg, src_epsg)           # apply re-reprojection and geojson to feature collection transfermation
-	fc2_asset_id = "disturbance_attributed_polygons_"+str(change_params['years']['start'])+'_'+str(change_params['years']['end'])
-	export_result = poly_attr.export_attributed_polygons(reprojected_geojson, asset_path, fc2_asset_id)   # save date in GEE
-	return 0 
-#-------------------------------------------------------------------
-def attribute_current_disturbance_polygons(poly_attr,change_params,asset_path,composite_params):
-	img_asset_id = "predictor_img_"+str(composite_params['start_date'].year)+'_'+str(composite_params['end_date'].year)
-	raster_stack = ee.Image(asset_path + img_asset_id)
-	fc1_asset_id = "disturbance_polygons_"+str(change_params['years']['start'])+'_'+str(change_params['years']['end'])
-	disturbance_polygons_asset2 = ee.FeatureCollection(asset_path+fc1_asset_id)
-	event_polygons_attri = poly_attr.attribute_with_reference_data()
-	fc2_asset_id = "disturbance_attributed_polygons_"+str(change_params['years']['start'])+'_'+str(change_params['years']['end'])
-	poly_attr.export_attributed_polygons(event_polygons_attri, asset_path, fc2_asset_id)
-	return 0 
-#-------------------------------------------------------------------
-def predict(params_handler,change_params,label_property,asset_path):
-
-	params_handler.update_change_params(delta='loss',sort='greatest', years={'start': 2008, 'end': 2012}, mag={'value': 200, 'operator': '>'}, dur={'value': 4, 'operator': '<'}, preval={'value': 300, 'operator': '>'}, mmu={'value': 15})
-	labeled = "disturbance_attributed_polygons_"+str(change_params['years']['start'])+'_'+str(change_params['years']['end'])
-	params_handler.update_change_params(delta='loss',sort='greatest', years={'start': 2019, 'end': 2024}, mag={'value': 200, 'operator': '>'}, dur={'value': 4, 'operator': '<'}, preval={'value': 300, 'operator': '>'}, mmu={'value': 15})
-	unlabeled = "disturbance_attributed_polygons_"+str(change_params['years']['start'])+'_'+str(change_params['years']['end'])
-	# Instantiate the classifier
-	classifier = bnet.FeatureClassifier(asset_path+labeled, asset_path+unlabeled, label_property)
-	# Train the classifier
-	trained_classifier = classifier.train_classifier()
-	# Classify the features
-	classified_fc = classifier.classify_features(trained_classifier)
-	# Print the first few classified features
-	classifier.print_classified_features(classified_fc)
-	# Export the classified FeatureCollection to Google Drive
-	classifier.export_classified(classified_fc,asset_path)
-	return 0
 
